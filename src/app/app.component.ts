419fb5c04dd85077e0beb85f0c2ca2a895aa151e
William Nascimento <william.nascimento@capgemini.com>
2021-09-14 22:50:28 -0300

    iniciando projeto todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: string = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'Fazer curso angular', true));
+  }
+
+  alterarTexto() {
+    this.title = 'teste';
+  }
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,8 @@
+export class Todo {
+    
+    constructor(
+        public id: Number,
+        public title: string,
+        public done: Boolean,
+    ) {}
+}
