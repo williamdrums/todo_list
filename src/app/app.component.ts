2978e1edf042dc7aad6a92fdcbcab45907bd4ccf
William Nascimento <william.nascimento@capgemini.com>
2021-09-17 00:31:24 -0300

    criando update task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   public todos: Todo[] = [];
   public title: string = 'Minhas Tarefas';
   public form: FormGroup;
+  public todo!: Todo;
 
   constructor(private formBuilder: FormBuilder) {
 
@@ -21,31 +22,63 @@ export class AppComponent {
         Validators.required
       ])]
     });
+
+    this.load();
   }
 
   add() {
     const title = this.form.controls['title'].value;
-    const id = this.todos.length + 1;
-    this.todos.push(new Todo(id, title, false));
-    this.clear();
+    const index = this.todos.indexOf(this.todo)
+  
+    if (index == -1) {
+      const id = this.todos.length + 1;
+      this.todos.push(new Todo(id, title, false));
+      this.save();
+      this.clear();
+    } else {
+      this.todos[index].title = title;
+      this.save();
+      this.clear();
+    }
   }
 
   clear() {
     this.form.reset();
   }
+
   remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
     if (index !== -1) {
-      //deleta item(indice e 1 quantidade de intem a ser deletado)
+      //deletar item(indice e 1 quantidade a ser deletado)
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
+  }
+
+  save() {
+    //convertendo JSON pra string
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    //convertentdo os dados pra json
+    this.todos = JSON.parse(data!);
+  }
+
+  update(todo: Todo) {
+    this.form.patchValue({ title: todo.title });
+    this.todo = todo;
   }
 }
